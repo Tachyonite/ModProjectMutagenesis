6ed9f91b4e9b70d93fd754a2c1e32c9aaf77d0dc
FxWolfxe <ndmuench@comcast.net>
2021-05-29 18:53:05 -0400

    started to create xsd files

---
+++ VSProject/ProjectMutagenesis/ProjectMutagenesis/MutagenesisInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMutagenesis
+{
+    /// <summary>
+    /// static class to initialize the mod
+    /// </summary>
+    public static class MutagenesisInit
+    {
+        static MutagenesisInit()
+        {
+            //TODO init
+        }
+
+    }
+}
